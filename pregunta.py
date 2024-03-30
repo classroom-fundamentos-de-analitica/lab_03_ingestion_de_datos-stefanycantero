3605ad5213be816361eaccbb5a1c9eba8045e2e2
Stefany Cantero <stefanycanteroc@gmail.com>
2024-03-30 18:49:15 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -23,9 +23,12 @@ def ingest_data():
                 data["cluster"].append(int(data_in_file[0]))
                 data["cantidad_de_palabras_clave"].append(int(data_in_file[1]))
                 data["porcentaje_de_palabras_clave"].append(float(data_in_file[2].replace(",", ".")))
-                data["principales_palabras_clave"].append(" ".join(data_in_file[4:]).replace(".", "").rstrip() + " ")
+                data["principales_palabras_clave"].append(" ".join(data_in_file[4:]).replace(".", "").strip() + " ")
             elif data_in_file:
                 data["principales_palabras_clave"][-1] += " ".join(data_in_file).replace(".", "").strip() + " "
+
+        # Eliminar el espacio al final de cada cadena de palabras clave
+        data["principales_palabras_clave"] = list(map(lambda x: x.strip(), data["principales_palabras_clave"]))
 
         df = pd.DataFrame(data)
 
