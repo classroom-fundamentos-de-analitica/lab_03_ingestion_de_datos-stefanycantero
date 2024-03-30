39f7b7909e8a45ccbf725a9ea6ef908508734d63
Stefany Cantero <stefanycanteroc@gmail.com>
2024-03-30 16:17:13 -0500

    aversiyasi

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,22 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
+    with open("clusters_report.txt", "r") as file:
+        data = {"cluster": [], "cantidad_de_palabras_clave": [], "porcentaje_de_palabras_clave": [], "principales_palabras_clave": []}
 
-    #
-    # Inserte su código aquí
-    #
+        for line in file.readlines()[4:]: # Se omiten las primeras cuatro lineas
+            line = line.replace("\n", "")
+            data_in_file = list(filter(lambda x: x != "", line.split(" ")))
+
+            if "%" in data_in_file:
+                data["cluster"].append(int(data_in_file[0]))
+                data["cantidad_de_palabras_clave"].append(int(data_in_file[1]))
+                data["porcentaje_de_palabras_clave"].append(float(data_in_file[2].replace(",", ".")))
+                data["principales_palabras_clave"].append(" ".join(data_in_file[3:]).replace(".", "").strip().replace("%", ""))
+            elif data_in_file:
+                data["principales_palabras_clave"][-1] += " ".join(data_in_file).replace(".", "").strip() + " "
+
+        df = pd.DataFrame(data)
 
     return df
