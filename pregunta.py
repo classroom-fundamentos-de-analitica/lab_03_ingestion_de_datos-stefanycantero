c06907133945b9a32e68ded3eccbff2e94f9f744
Stefany Cantero <stefanycanteroc@gmail.com>
2024-03-30 17:26:45 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,7 +15,7 @@ def ingest_data():
     with open("clusters_report.txt", "r") as file:
         data = {"cluster": [], "cantidad_de_palabras_clave": [], "porcentaje_de_palabras_clave": [], "principales_palabras_clave": []}
 
-        for line in file.readlines()[4:]: # Se omiten las primeras cuatro lineas
+        for line in file.readlines()[4:]:
             line = line.replace("\n", "")
             data_in_file = list(filter(lambda x: x != "", line.split(" ")))
 
@@ -23,12 +23,12 @@ def ingest_data():
                 data["cluster"].append(int(data_in_file[0]))
                 data["cantidad_de_palabras_clave"].append(int(data_in_file[1]))
                 data["porcentaje_de_palabras_clave"].append(float(data_in_file[2].replace(",", ".")))
-                data["principales_palabras_clave"].append(" ".join(data_in_file[4:]).replace(".", "").strip())
+                data["principales_palabras_clave"].append(" ".join(data_in_file[4:]).replace(".", "").strip() + " ")
             elif data_in_file:
-                data["principales_palabras_clave"][-1] += " ".join(data_in_file).replace(".", "").strip()
+                data["principales_palabras_clave"][-1] += " ".join(data_in_file).replace(".", "").strip() + " "
 
         df = pd.DataFrame(data)
 
     return df
 
-# print(ingest_data().principales_palabras_clave.to_list()[12])
+# print(ingest_data().principales_palabras_clave.to_list()[0])
